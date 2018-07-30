afa7a67db2427b6882d831aa77baf10d60a8c218
ysl <1071267286@qq.com>
2018-07-30 10:24:02 +0800

    index-icons

--- ysl/src/App.vue
+++ ysl/src/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app">
     <home-header></home-header>
-    <home-swiper></home-swiper>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
   </div>
 </template>
 
 <script>
   import HomeHeader from 'components/HomeHeader'
   import HomeSwiper from 'components/HomeSwiper'
+  import HomeIcons from 'components/HomeIcons'
+  import axios from 'axios'
   export default {
     name: 'App',
     components: {
       HomeHeader,
-      HomeSwiper
-    }
+      HomeSwiper,
+      HomeIcons
+    },
+    data () {
+      return {
+        swiperList: [],
+        iconList: [],
+        recommendList: [],
+        weekendList: []
+      }
+    },
+    methods: {
+      getHomeInfo () {
+        axios.get('https://easy-mock.com/mock/5b3eba9c04502462c6feb598/ysl/qunaer')
+          .then(this.getHomeInfoSucc)
+      },
+      getHomeInfoSucc (res) {
+        res = res.data
+        if (res.ret && res.data) {
+          const data = res.data
+          this.swiperList = data.swiperList
+          console.log(this.swiperList)
+          this.iconList = data.iconList
+          this.recommendList = data.recommendList
+          this.weekendList = data.weekendList
+        }
+      }
+    },
+    mounted() {
+      this.getHomeInfo()
+    },
   }
 </script>
 
--- ysl/src/components/HomeSwiper.vue
+++ ysl/src/components/HomeSwiper.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="item of swiperList" :key="item.id">
-        <img :src="item.url" width="100%">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
@@ -10,35 +10,37 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        swiperOption: {
-          pagination: '.swiper-pagination',
-          loop: true
-        },
-        swiperList: [
-          {
-            id: '001',
-            url: 'https://img1.qunarzz.com/qs/1807/e0/e349cd7cdbd92602.jpg'
-          },
-          {
-            id: '002',
-            url: 'https://img1.qunarzz.com/qs/1807/db/e8ccd94f91ec9702.jpg'
-          }
-        ]
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+          autoplay: 2000,
+        loop: true
       }
     }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
   }
+}
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active
-    background-color #fff
+    background: #fff
   .wrapper
-    overflow hidden
-    width 100%
-    height 0
-    padding-bottom 54%
-    background-color #ccc
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
 </style>
