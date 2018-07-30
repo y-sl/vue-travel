31eb76183cb5076b0af40ba38e014011078d6a0a
ysl <1071267286@qq.com>
2018-07-30 13:48:03 +0800

    dist

---
+++ ysl/src/components/PageHome.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <home-header></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
+  </div>
+</template>
+
+<script>
+  import HomeHeader from 'components/HomeHeader'
+  import HomeSwiper from 'components/HomeSwiper'
+  import HomeIcons from 'components/HomeIcons'
+  import HomeRecommend from 'components/HomeRecommend'
+  import HomeWeekend from 'components/HomeWeekend.vue'
+  import axios from 'axios'
+  export default {
+    name: 'home',
+    components: {
+      HomeHeader,
+      HomeSwiper,
+      HomeIcons,
+      HomeRecommend,
+      HomeWeekend
+    },
+    data () {
+      return {
+        swiperList: [],
+        iconList: [],
+        recommendList: [],
+        weekendList: []
+      }
+    },
+    methods: {
+      getHomeInfo () {
+        axios.get('https://easy-mock.com/mock/5b3eba9c04502462c6feb598/ysl/qunaer')
+          .then(this.getHomeInfoSucc)
+      },
+      getHomeInfoSucc (res) {
+        res = res.data
+        if (res.ret && res.data) {
+          const data = res.data
+          this.swiperList = data.swiperList
+          console.log(this.swiperList)
+          this.iconList = data.iconList
+          this.recommendList = data.recommendList
+          this.weekendList = data.weekendList
+        }
+      }
+    },
+    mounted() {
+      this.getHomeInfo()
+    },
+  }
+</script>
+
+<style>
+
+</style>
--- ysl/src/router/index.js
+++ ysl/src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import PageHome from 'components/PageHome'
+import PageCity from 'components/PageCity'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-   
+   {
+    path: '/',
+    component: PageHome
+   },
+   {
+     path: '/city',
+     component: PageCity
+   }
   ]
 })
